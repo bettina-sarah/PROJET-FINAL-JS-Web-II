fa69603fc27c9a36ab7bdab17159b434490db78d
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-01-24 10:19:46 -0500

    hover, button, error msg, ai image flash, fineprint

---
+++ client/src/forestSpirit.js
@@ -0,0 +1,10 @@
+class forestSpirit {
+    constructor() {
+        
+
+    }
+
+    tick(){
+
+    }
+}
--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,14 +1,23 @@
 
-let bodyNode;
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+let wrapperNode;
+let imageDiv;
+let passwordField;
+let submitButton;
 
 
 window.addEventListener("load", () => {
+    
     passwordValidation();
     
 })
 
 const passwordValidation = () => {
 
+    passwordField = document.querySelector("#password");
+    submitButton = document.querySelector("#submit");
+                    
     document.querySelector("#password-form").onsubmit = () => {
         let success = true;
 
@@ -17,21 +26,55 @@ const passwordValidation = () => {
             console.log("false")
             document.querySelector("#error-message").style.display = "block";
             annoyingImage();
+            passwordField.disabled = true;
+            submitButton.disabled = true;
+            finePrint();
         }
-
-        //erreur d'identification a cacher...
 
         return success;
     }
 }
 
 const annoyingImage = () => {
-    bodyNode = document.querySelector("#index")
-    let element = document.createElement("div");
-    element.classList.add("annoying-image");
+    wrapperNode = document.querySelector("#wrapper");
+    imageDiv = document.createElement("div");
+    imageDiv.classList.add("annoying-image");
+    imageDiv.style.top = Math.random()*innerHeight - 500 + "px";
+    imageDiv.style.left = Math.random()*innerWidth - 500 + "px";
+    wrapperNode.append(imageDiv);
 
-    bodyNode.append(element);
+    setInterval(float, 1000);
+}
+
+const float = () => {
+
+    imageDiv.style.top = Math.random()*innerHeight + "px";
+    imageDiv.style.left = Math.random()*innerWidth + "px";
 
     
+    //window.requestAnimationFrame(float);
+}
+
+const finePrint = () => {
+    let finePrint = document.createElement("div");
+    finePrint.classList.add("fine-print");
+    finePrint.innerText = "Appuyez ici pour réactiver le mot de passe";
+
+    finePrint.style.bottom = "0px";
+    finePrint.style.right = "0px";
+    wrapperNode.append(finePrint);
+
+    finePrint.onclick = () => {
+        finePrint.style.color = "rgb(26, 151, 109)";
+        finePrint.style.boxShadow = "5px 10px rgb(26, 151, 109)";
+        passwordField.disabled = false;
+        submitButton.disabled = false;
+        imageDiv.remove();
+
+        //le div fine-print s'enleve pas tout de suite
+        setTimeout( () => {
+            finePrint.remove();}, 1000);
+        
+    }
 
 }
